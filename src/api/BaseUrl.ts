cef3e957b0729d179dd242bd2b3ecdaa93de1553
Pedro Lucas <pedroldima52@gmail.com>
2026-02-13 22:31:14 -0300

    fix error in api url

--- src/api/BaseUrl.ts
+++ src/api/BaseUrl.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // URL LOCAL
 // const url = "http://localhost:3000/api";
-const url = "https://api-saloon.onrender.com/api";
+const url = "https://estudos-ruby-1.onrender.com/api";
 
 const api = axios.create({
   baseURL: url,
